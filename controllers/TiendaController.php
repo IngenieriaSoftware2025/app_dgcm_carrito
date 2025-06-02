1771287e886c03d03a487aee8976e929cb87907e
Carbifa <carbyfah@gmail.com>
2025-06-02 11:33:16 -0600

    Controlador para obtener productos en TiendaController

--- controllers/TiendaController.php
+++ controllers/TiendaController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Controllers;
+
 use Exception;
 use MVC\Router;
 use Model\Facturas;
@@ -12,5 +13,29 @@ class TiendaController extends ActiveRecord
     {
         $router->render('tienda/index', []);
     }
-}
 
+    public static function buscarAPI() {
+        getHeadersApi();
+
+        try{
+            $consultaParaBuscar = "SELECT * FROM productos WHERE producto_situacion = 1 ORDER BY producto_cantidad";
+            $productoBuscar =  self::fetchArray($consultaParaBuscar);
+
+            http_response_code(200);
+            echo json_encode([
+                'codigo' => 1,
+                'mensaje' => 'Exito al obtener Productos',
+                'data' => $productoBuscar
+            ]);
+            return;
+        }catch(Exception $e){
+            http_response_code(400);
+            echo json_encode([
+                'codigo' => 0,
+                'mensaje' => 'Error al obtener Productos',
+                'detalle' => $e->getMessage()
+            ]);
+        }
+        
+    }
+}
